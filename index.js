28a8beb663ac628718d9914d90d1015f973ab684
Martin Larsson <martin.larsson.post@gmail.com>
2017-10-24 19:42:27 +0200

    updated index

--- index.js
+++ index.js
@@ -26,3 +26,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+window.onscroll = function() {
+	if (window.scrollY >= 700 && window.scrollY < 1100){
+        document.getElementById('startLink').className = '';
+        document.getElementById('projektLink').className = 'active';
+		document.getElementById('kontaktLink').className = '';
+    }  
+    else if (window.scrollY >= 1100 ){
+       document.getElementById('startLink').className = '';
+        document.getElementById('projektLink').className = '';
+        document.getElementById('kontaktLink').className = 'active';
+    }
+	else if (window.scrollY < 400 ){
+       document.getElementById('startLink').className = 'active';
+        document.getElementById('projektLink').className = '';
+        document.getElementById('kontaktLink').className = ''; 
+    }
+}
